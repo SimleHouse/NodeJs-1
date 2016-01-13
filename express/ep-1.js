083f5e71138753b494b358705263a49db6590b1e
笑耗子ok <xuhao20088002@163.com>
2016-01-13 17:32:46 +0800

    添加了连接mongodb和插入数据的操作

--- express/ep-1.js
+++ express/ep-1.js
@@ -2,8 +2,31 @@
  * Created by talent on 2016/1/11.
  */
 var express=require('express');
-console.log(express);
+var app=express();
+var mongodb=require('mongodb');
+var mongoServer=new mongodb.Server('127.0.0.1',27017,{auto_reconnect:true});
+var db=new mongodb.Db('testMongodb',mongoServer,{safe:true});//创建一张表
+db.open(function(err,db){
+    if(err){
+        console.log('数据库连接错误');
+    }else{
+        db.createCollection('basicIfo',{safe:true},function(err,collection){
+            if(err){
+                console.log('连接表testMondodb失败');
+            }else{
+                //var data=[{name:'nima',password:'10080'}];
+                collection.insertOne({name:'nima',password:'10080'},function(err,result){
+                    if(err){
+                        console.log('Error:0'+err);
+                    }else{
+                        console.log(result);
+                    }
+                })
+            }
+        });
+    }
+    console.log(arguments);
+});
 
 
 
-console.log('hello');
